5462728af4a17f6d40500795e50f4f37b7761c77
Alex Klyuev <aklyuev96@gmail.com>
2021-12-27 19:16:42 -0500

    update rpc function

--- rpc.js
+++ rpc.js
@@ -1,11 +1,5 @@
 const axios = require('axios');
-const { user, password, network } = require('./nodeDetails.js');
-
-// set to 'local' if node runs on same machine
-// set to 'lan' if node runs on another machine on local network
-const node = 'local';
-
-const { adr, port } = network[node];
+const { user, password, adr, port } = require('./nodeDetails.js');
 
 const rpc = async (params) => {
   const res = await axios.post(`http://${user}:${password}@${adr}:${port}`, params);
