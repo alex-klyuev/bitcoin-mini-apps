4b37f06fa23a877ef4024941164e9f25a61088bf
Alex Klyuev <aklyuev96@gmail.com>
2021-12-27 16:27:16 -0800

    add error handling for rpc calls

--- rpc.js
+++ rpc.js
@@ -2,8 +2,12 @@ const axios = require('axios');
 const { user, password, adr, port } = require('./nodeDetails.js');
 
 const rpc = async (params) => {
-  const res = await axios.post(`http://${user}:${password}@${adr}:${port}`, params);
-  return res.data;
+  try {
+    const res = await axios.post(`http://${user}:${password}@${adr}:${port}`, params);
+    return res.data
+  } catch (err) {
+    return err;
+  }
 };
 
 module.exports = rpc;
